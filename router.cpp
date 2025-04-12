b59f2d443403517e7a7243da5c27e722f3754841
Stefan <stefan.ghinescu2307@stud.acs.upb.ro>
2025-04-12 14:58:11 +0000

    hope refactor gud

--- router.cpp
+++ router.cpp
@@ -1,92 +1,4 @@
-#include "protocols.h"
-// #include "queue.h"
-#include "lib.h"
-#include <arpa/inet.h>	
-#include <cstring>
-#include <arpa/inet.h>
-
-#include <array>
-#include <algorithm>
-
-#define ETHER_TYPE_IP 0x0800
-#define ETHER_TYPE_ARP 0x0806
-
-#define RTABLE_SIZE 80000
-#define ARP_TABLE_SIZE 10
-
-typedef ether_hdr ethernet_header;
-typedef ip_hdr ipv4_header;
-typedef arp_hdr arp_header;
-typedef icmp_hdr icmp_header;
-
-struct ethernet_frame
-{
-	ethernet_header eth_hdr;
-	uint8_t payload[MAX_PACKET_LEN - sizeof(ethernet_header)];
-};
-
-struct ipv4_packet
-{
-	ethernet_header eth_hdr;
-	ipv4_header ipv4_hdr;
-	uint8_t payload[MAX_PACKET_LEN - sizeof(ethernet_header) - sizeof(ip_hdr)];
-};
-
-struct arp_packet
-{
-	ethernet_header eth_hdr;
-	arp_header arp_hdr;
-	uint8_t payload[MAX_PACKET_LEN - sizeof(ethernet_header) - sizeof(arp_header)];
-};
-
-struct icmp_packet
-{
-	ethernet_header eth_hdr;
-	ipv4_header ipv4_hdr;
-	icmp_header icmp_hdr;
-	uint8_t payload[MAX_PACKET_LEN - sizeof(ethernet_header) - sizeof(ip_hdr) - sizeof(icmp_header)];
-};
-
-
-route_table_entry* get_best_route(uint32_t ip_dest, route_table_entry* rtable, size_t rtable_len)
-{
-	for (size_t i = 0; i < rtable_len; i++) {
-		if (rtable[i].prefix == (ip_dest & rtable[i].mask)) {
-			return &rtable[i];
-		}
-	}
-	return NULL;
-}
-
-arp_table_entry* get_arp_entry(uint32_t ip_dest, arp_table_entry* arp_table, size_t arp_table_len)
-{
-	for (size_t i = 0; i < arp_table_len; i++) {
-		if (arp_table[i].ip == ip_dest) {
-			return &arp_table[i];
-		}
-	}
-	return NULL;
-}
-
-route_table_entry* get_best_route_array(uint32_t ip_dest, std::array<route_table_entry, RTABLE_SIZE>& rtable_array)
-{
-	for (size_t i = 0; i < rtable_array.size(); i++) {
-		if (rtable_array[i].prefix == (ip_dest & rtable_array[i].mask)) {
-			return &rtable_array[i];
-		}
-	}
-	return NULL;
-}
-
-arp_table_entry* get_arp_entry_array(uint32_t ip_dest, std::array<arp_table_entry, ARP_TABLE_SIZE>& arp_table_array)
-{
-	for (size_t i = 0; i < arp_table_array.size(); i++) {
-		if (arp_table_array[i].ip == ip_dest) {
-			return &arp_table_array[i];
-		}
-	}
-	return NULL;
-}
+#include "router.hpp"
 
 char* my_inet_ntoa(uint32_t ip)
 {
@@ -106,7 +18,6 @@ char *my_mac_ntoa(uint8_t *mac)
 	return tmp;
 }
 
-
 bool cmp(route_table_entry& a, route_table_entry& b)
 {
 	if (a.mask == b.mask) {
@@ -115,18 +26,58 @@ bool cmp(route_table_entry& a, route_table_entry& b)
 	return a.mask > b.mask;
 }
 
-void handle_ip_packet(char buf[MAX_PACKET_LEN], size_t len, size_t interface,
+void handle_time_exceeded(icmp_packet* icmp_packet)
+{
+
+}
+
+void handle_ipv4_packet(ipv4_packet* ipv4_packet,
 					  std::array<route_table_entry, RTABLE_SIZE>& rtable_array,
 					  size_t rtable_len,
 					  std::array<arp_table_entry, ARP_TABLE_SIZE>& arp_table_array,
 					  size_t arp_table_len)
 {
+	printf("Am primit IP packet\n");
+	printf("Sursa este %s, iar destinatia este %s\n", my_inet_ntoa(ipv4_packet->get_src_ip()), my_inet_ntoa(ipv4_packet->get_dest_ip()));
+	if (!ipv4_packet->is_valid())
+	{
+		printf("Pachet IP invalid, checksum prost\n");
+		return;
+	}
+	printf("Avem checksum bun\n");
 
+	if (!ipv4_packet->is_not_expired())
+	{
+		printf("TTL expired\n");
+		handle_time_exceeded(reinterpret_cast<icmp_packet *>(ipv4_packet));
+		return;
+	}
+	ipv4_packet->calculate_checksum();
+	printf("TTL este %d\n", ipv4_packet->get_ttl());
+
+	route_table_entry* best_route = get_best_route_array(ipv4_packet->get_dest_ip(), rtable_array);
+	if (best_route == NULL)
+	{
+		printf("Nu am gasit ruta\n");
+		return;
+	}
+	printf("Am gasit ruta si next-hop este %s\n", my_inet_ntoa(best_route->next_hop));
+
+	arp_table_entry* arp_entry = get_arp_entry_array(best_route->next_hop, arp_table_array);
+	if (arp_entry == NULL)
+	{
+		printf("Nu am gasit entry in ARP\n");
+		return;
+	}
+
+	ipv4_packet->send_to_route(best_route, arp_entry);
+
+	printf("Am gasit entry in ARP, src = %s dst = %s\n", my_mac_ntoa(ipv4_packet->get_src_mac()), my_mac_ntoa(arp_entry->mac));
 }
 
 int main(int argc, char *argv[])
 {
-	char buf[MAX_PACKET_LEN];
+	ethernet_frame eth_frame;
 
 	// Do not modify this line
 	init(argv + 2, argc - 2);
@@ -151,90 +102,27 @@ int main(int argc, char *argv[])
 	while (1)
 	{
 		size_t interface;
-		size_t len;
+		// size_t len;
 
-		interface = recv_from_any_link(buf, &len);
+		interface = recv_from_any_link(eth_frame.buf, &eth_frame.length);
 		DIE(interface < 0, "recv_from_any_links");
 
 		printf("\n\nrouter %zu\n", ++cnt);
 
-		// ethernet_frame* eth_frame = (ethernet_frame *)buf;
-		// uint16_t eth_type = ntohs(eth_frame->eth_hdr.ethr_type);
+		ethernet_frame ethernet_frame;
+		uint16_t eth_type = eth_frame.get_ether_type();
 
-		ether_hdr* eth_header = reinterpret_cast<ether_hdr *>(buf);
-		void* payload = (void *)(buf + sizeof(ether_hdr));
+		// ether_hdr* eth_header = (ether_hdr *)buf;
+		// void* payload = (void *)(buf + sizeof(ether_hdr));
 
-		printf("Am primit frame cu src = %s si dest = %s\nde tip = %x\n", my_mac_ntoa(eth_header->ethr_shost), my_mac_ntoa(eth_header->ethr_dhost), ntohs(eth_header->ethr_type));
-		uint16_t l_ether_type = ntohs(eth_header->ethr_type);
+		printf("Am primit frame cu src = %s si dest = %s\nde tip = %x\n", my_mac_ntoa(eth_frame.get_src_mac()), my_mac_ntoa(eth_frame.get_dest_mac()), ntohs(eth_frame.get_ether_type()));
+		// uint16_t l_ether_type = ntohs(eth_header->ethr_type);
 
-		switch (l_ether_type)
+		switch (eth_type)
 		{
 		case ETHER_TYPE_IP:
 		{
-			printf("Am primit IP packet\n");
-			
-			ip_hdr* ip_header = (ip_hdr* )payload;
-
-			uint16_t packet_checksum = ip_header->checksum;
-			packet_checksum = ntohs(packet_checksum);
-			ip_header->checksum = 0;
-			uint16_t calculated_checksum = checksum((uint16_t* )ip_header, sizeof(ip_hdr));
-			if (packet_checksum != calculated_checksum)
-			{
-				continue;
-			}
-			ip_header->checksum = htons(packet_checksum);
-			printf("Avem checksum bun\n");
-
-			uint32_t dest_address = ip_header->dest_addr;
-			route_table_entry* best_route = get_best_route_array(dest_address, rtable_array);
-
-			printf("Sursa este %s, iar destinatia este %s\n", my_inet_ntoa(ip_header->source_addr), my_inet_ntoa(dest_address));
-
-			if (best_route == NULL)
-			{
-				printf("Nu am gasit ruta\n");
-				continue;
-			}
-			
-			dest_address = best_route->next_hop;
-			printf("Am gasit ruta si next-hop este %s\n", my_inet_ntoa(dest_address));
-
-			printf("best route:\n\tprefix = %s next_hop = %s mask = %s interface = %d\n", my_inet_ntoa(best_route->prefix), my_inet_ntoa(best_route->next_hop), my_inet_ntoa(best_route->mask), best_route->interface);
-
-			if (ip_header->ttl >= 1)
-			{
-				ip_header->ttl = ip_header->ttl - 1;
-			}
-			else
-			{
-				printf("TTL expired\n");
-				continue;
-			}
-
-			printf("TTL este %d\n", ip_header->ttl);
-
-			// ip_header->checksum = htons(~(~ntohs(ip_header->checksum) + ~((uint16_t)((ip_header->ttl) + 1)) + (uint16_t)(ip_header->ttl)) - 1);
-			ip_header->checksum = 0;
-			ip_header->checksum = htons(checksum((uint16_t*)ip_header, sizeof(ip_hdr)));
-
-			arp_table_entry* arp_entry = get_arp_entry_array(dest_address, arp_table_array);
-			if (arp_entry == NULL)
-			{
-				printf("Nu am gasit entry in ARP\n");
-				continue;
-			}
-
-			uint8_t src_mac[6] = {0};
-			get_interface_mac(best_route->interface, src_mac);
-
-			memcpy(eth_header->ethr_shost, src_mac, 6);
-			memcpy(eth_header->ethr_dhost, arp_entry->mac, 6);
-
-			printf("Am gasit entry in ARP, src = %s dst = %s\n", my_mac_ntoa(src_mac), my_mac_ntoa(arp_entry->mac));
-
-			send_to_link(len, buf, best_route->interface);
-
+			handle_ipv4_packet(reinterpret_cast<ipv4_packet *>(&eth_frame), rtable_array, rtable_len, arp_table_array, arp_table_len);
 			break;
 		}
 
@@ -242,40 +130,40 @@ int main(int argc, char *argv[])
 		{
 			printf("Am primit ARP packet\n");
 
-			arp_hdr* arp_header = (arp_hdr* )payload;
+			// arp_hdr* arp_header = (arp_hdr* )eth_frame->payload;
 
-			uint16_t opcode = ntohs(arp_header->opcode);
-			if (opcode == 1)
-			{
-				printf("Am primit ARP request\n");
-				uint32_t dest_ip = arp_header->tprotoa;
-				uint8_t dest_mac[6] = {0};
-				uint32_t src_ip = arp_header->sprotoa;
-				uint8_t src_mac[6] = {0};
+			// uint16_t opcode = ntohs(arp_header->opcode);
+			// if (opcode == 1)
+			// {
+			// 	printf("Am primit ARP request\n");
+			// 	uint32_t dest_ip = arp_header->tprotoa;
+			// 	uint8_t dest_mac[6] = {0};
+			// 	uint32_t src_ip = arp_header->sprotoa;
+			// 	uint8_t src_mac[6] = {0};
 
-				printf("\tsrc_mac = %s src_ip = %s\n\tdest_mac = %s dest_ip = %s\n",
-					my_mac_ntoa(arp_header->shwa), my_inet_ntoa(src_ip),
-					my_mac_ntoa(arp_header->thwa), my_inet_ntoa(dest_ip));
+			// 	printf("\tsrc_mac = %s src_ip = %s\n\tdest_mac = %s dest_ip = %s\n",
+			// 		my_mac_ntoa(arp_header->shwa), my_inet_ntoa(src_ip),
+			// 		my_mac_ntoa(arp_header->thwa), my_inet_ntoa(dest_ip));
 				
-			}
-			else if (opcode == 2)
-			{
-				printf("Am primit ARP reply\n");
+			// }
+			// else if (opcode == 2)
+			// {
+			// 	printf("Am primit ARP reply\n");
 
-				uint32_t dest_ip = arp_header->tprotoa;
-				uint8_t dest_mac[6] = {0};
-				uint32_t src_ip = arp_header->sprotoa;
-				uint8_t src_mac[6] = {0};
+			// 	uint32_t dest_ip = arp_header->tprotoa;
+			// 	uint8_t dest_mac[6] = {0};
+			// 	uint32_t src_ip = arp_header->sprotoa;
+			// 	uint8_t src_mac[6] = {0};
 
-				printf("\tsrc_mac = %s src_ip = %s\n\tdest_mac = %s dest_ip = %s\n",
-					my_mac_ntoa(arp_header->shwa), my_inet_ntoa(src_ip),
-					my_mac_ntoa(arp_header->thwa), my_inet_ntoa(dest_ip));
-			}
-			else
-			{
-				printf("Am primit un ARP invalid\n");
-				continue;
-			}
+			// 	printf("\tsrc_mac = %s src_ip = %s\n\tdest_mac = %s dest_ip = %s\n",
+			// 		my_mac_ntoa(arp_header->shwa), my_inet_ntoa(src_ip),
+			// 		my_mac_ntoa(arp_header->thwa), my_inet_ntoa(dest_ip));
+			// }
+			// else
+			// {
+			// 	printf("Am primit un ARP invalid\n");
+			// 	continue;
+			// }
 		}
 		
 		default:
--- router.hpp
+++ router.hpp
@@ -1,9 +1,7 @@
 #include "protocols.h"
-// #include "queue.h"
 #include "lib.h"
 #include <arpa/inet.h>	
 #include <cstring>
-#include <arpa/inet.h>
 
 #include <array>
 #include <algorithm>
@@ -19,30 +17,153 @@ typedef ip_hdr ipv4_header;
 typedef arp_hdr arp_header;
 typedef icmp_hdr icmp_header;
 
+route_table_entry* get_best_route(uint32_t ip_dest, route_table_entry* rtable, size_t rtable_len)
+{
+	for (size_t i = 0; i < rtable_len; i++) {
+		if (rtable[i].prefix == (ip_dest & rtable[i].mask)) {
+			return &rtable[i];
+		}
+	}
+	return NULL;
+}
+
+arp_table_entry* get_arp_entry(uint32_t ip_dest, arp_table_entry* arp_table, size_t arp_table_len)
+{
+	for (size_t i = 0; i < arp_table_len; i++) {
+		if (arp_table[i].ip == ip_dest) {
+			return &arp_table[i];
+		}
+	}
+	return NULL;
+}
+
+route_table_entry* get_best_route_array(uint32_t ip_dest, std::array<route_table_entry, RTABLE_SIZE>& rtable_array)
+{
+	for (size_t i = 0; i < rtable_array.size(); i++) {
+		if (rtable_array[i].prefix == (ip_dest & rtable_array[i].mask)) {
+			return &rtable_array[i];
+		}
+	}
+	return NULL;
+}
+
+arp_table_entry* get_arp_entry_array(uint32_t ip_dest, std::array<arp_table_entry, ARP_TABLE_SIZE>& arp_table_array)
+{
+	for (size_t i = 0; i < arp_table_array.size(); i++) {
+		if (arp_table_array[i].ip == ip_dest) {
+			return &arp_table_array[i];
+		}
+	}
+	return NULL;
+}
+
 struct ethernet_frame
 {
-	ethernet_header eth_hdr;
-	uint8_t payload[MAX_PACKET_LEN - sizeof(ethernet_header)];
+    char buf[MAX_PACKET_LEN];
+    size_t length;
+#define eth_hdr ((ethernet_header *)this->buf)
+
+    inline uint8_t* get_src_mac()
+    {
+        return eth_hdr->ethr_shost;
+    }
+
+    inline uint8_t* get_dest_mac()
+    {
+        return eth_hdr->ethr_dhost;
+    }
+
+    inline uint16_t get_ether_type()
+    {
+        return ntohs(eth_hdr->ethr_type);
+    }
 };
 
 struct ipv4_packet
 {
-	ethernet_header eth_hdr;
-	ipv4_header ipv4_hdr;
-	uint8_t payload[MAX_PACKET_LEN - sizeof(ethernet_header) - sizeof(ip_hdr)];
+    char buf[MAX_PACKET_LEN];
+    size_t length;
+#define eth_hdr ((ethernet_header *)this->buf)
+#define ipv4_hdr ((ipv4_header *)((char *)this->buf + sizeof(ethernet_header)))
+
+    inline uint8_t* get_src_mac() const
+    {
+        return eth_hdr->ethr_shost;
+    }
+    inline uint8_t* get_dest_mac() const
+    {
+        return eth_hdr->ethr_dhost;
+    }
+    inline uint16_t get_ether_type() const
+    {
+        return ntohs(eth_hdr->ethr_type);
+    }
+
+    inline uint32_t get_src_ip() const
+    {
+        return ipv4_hdr->source_addr;
+    }
+    inline uint32_t get_dest_ip() const
+    {
+        return ipv4_hdr->dest_addr;
+    }
+
+    inline uint8_t get_ttl() const
+    {
+        return ipv4_hdr->ttl;
+    }
+
+    inline void calculate_checksum()
+    {
+        ipv4_hdr->checksum = 0;
+        ipv4_hdr->checksum = htons(checksum(reinterpret_cast<uint16_t *>(ipv4_hdr), sizeof(ipv4_header)));
+    }
+
+    inline bool is_valid()
+    {
+        uint16_t packet_checksum = ipv4_hdr->checksum;
+        calculate_checksum();
+        uint16_t calculated_checksum = ipv4_hdr->checksum;
+        printf("Checksum: %x %x\n", packet_checksum, calculated_checksum);
+        return packet_checksum == calculated_checksum;
+    }
+ 
+    inline bool is_not_expired()
+    {
+        return --ipv4_hdr->ttl > 0;
+    }
+
+    inline route_table_entry* get_best_route(std::array<route_table_entry, RTABLE_SIZE>& rtable) const
+    {
+        return ::get_best_route_array(ipv4_hdr->dest_addr, rtable);
+    }
+
+    inline arp_table_entry* get_arp_entry(std::array<arp_table_entry, ARP_TABLE_SIZE>& arp_table) const
+    {
+        return ::get_arp_entry_array(ipv4_hdr->dest_addr, arp_table);
+    }
+
+    inline void send_to_route(route_table_entry* route, arp_table_entry* arp_entry)
+    {
+        uint8_t src_mac[6] = {0};
+        get_interface_mac(route->interface, src_mac);
+
+        memcpy(eth_hdr->ethr_shost, src_mac, 6);
+        memcpy(eth_hdr->ethr_dhost, arp_entry->mac, 6);
+
+        send_to_link(length, buf, route->interface);
+    }
+
 };
 
 struct arp_packet
 {
-	ethernet_header eth_hdr;
-	arp_header arp_hdr;
-	uint8_t payload[MAX_PACKET_LEN - sizeof(ethernet_header) - sizeof(arp_header)];
+    char buf[MAX_PACKET_LEN];
+    size_t length;
 };
 
 struct icmp_packet
 {
-	ethernet_header eth_hdr;
-	ipv4_header ipv4_hdr;
-	icmp_header icmp_hdr;
-	uint8_t payload[MAX_PACKET_LEN - sizeof(ethernet_header) - sizeof(ip_hdr) - sizeof(icmp_header)];
+    char buf[MAX_PACKET_LEN];
+    size_t length;
 };
